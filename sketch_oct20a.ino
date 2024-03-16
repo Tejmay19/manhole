afc69382962482df8d113850317e3c1fa6814d55
Tejmay <163103836+Tejmay19@users.noreply.github.com>
2024-03-16 12:03:03 +0530

    Add files via upload

---
+++ sketch_oct20a.ino
@@ -0,0 +1,24 @@
+//program for arduino and sensor
+#include<Mouse.h>
+const int trigpin=8;
+const int echopin=7;
+long duration;
+int distance;
+
+void setup() {
+    
+    pinMode(trigpin,OUTPUT);
+    pinMode(echopin,INPUT);
+    Serial.begin(9600);
+}
+
+void loop() {
+  digitalWrite(trigpin,HIGH);
+  //delayMicroseconds(10);
+  delayMicroseconds(1000000);
+  digitalWrite(trigpin,LOW);
+  duration=pulseIn(echopin,HIGH);
+  distance=duration*0.034/2;
+  Serial.println(distance);
+    
+}
